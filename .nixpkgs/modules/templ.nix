946e720dd4175be47c347acff4f5330ea05a47bf
joerdav <joe.davidson.21111@gmail.com>
2022-10-14 12:08:17 +0100

    Fri 14 Oct 2022 12:08:17 BST

--- .nixpkgs/modules/templ.nix
+++ .nixpkgs/modules/templ.nix
@@ -13,7 +13,7 @@ let
       sha256 = "1m865kbpbpw7w64awh1b915xzqw4hz5mj46jdf7jaf3zvqrm30d7";
     };
 
-    vendorSha256 = "00i5g8ab9ndlh9pq1q7c6md7xmzpvj7nq30b45ra2c5gf2cymrar";
+    modSha256 = "00i5g8ab9ndlh9pq1q7c6md7xmzpvj7nq30b45ra2c5gf2cymrar";
   };
 in
 {
